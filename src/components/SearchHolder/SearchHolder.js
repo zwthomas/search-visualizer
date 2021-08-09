8e528bba8c6dba20a52f297e8db65fa659d6d56e
Zachary Thomas <zwthomas13@gmail.com>
2021-08-09 10:25:56 -0500

    draggable walls

--- src/components/SearchHolder/SearchHolder.js
+++ src/components/SearchHolder/SearchHolder.js
@@ -1,19 +1,32 @@
+import React, { useState } from 'react';
 import "./SearchHolder.css"
 
 
 function SearchHolder() {
+    
+    let [color, setColor] = useState("blue")
     let dim = 20
     let template = []
+    let startingColors = []
+    for (let i = 0; i < dim; i++) {
+        let tempColors = []
+        for (let j = 0; j < dim; j++) {
+            tempColors.push("white")
+        }
+        startingColors.push(tempColors)
+    }
+    let [colors, setColors] = useState(startingColors)
+    let [mouseDown, setMouseDown] = useState(false);
+
     for (let i = 0; i < dim; i++) {
         template.push(<div className="grid"/>)
     }
-    console.log(template)
     return (
         <div className="search">
-            {template.map(element => (
+            {template.map((element, row) => (
                 <div className="grid-row">
-                    {template.map(element => (
-                        <div className="grid" onClick={() => alert("bitch")}/>
+                    {template.map((element, col) => (
+                        <div className="grid" row={row} col={col} style={{"backgroundColor": colors[row][col]}} onMouseEnter={(e) => {handleMouseEnter(mouseDown, e, colors, setColors)}} onMouseDown={(e) => {setMouseDown(true)}} onMouseUp={(e) => {setMouseDown(false)}}/>
                     ))}
                 </div>
             
@@ -22,6 +35,18 @@ function SearchHolder() {
            
         </div>
     )
+}
+
+function handleMouseEnter(mouseDown, e, colors, setColors) {
+    let row = e.target.attributes.row.value
+    let col = e.target.attributes.col.value
+
+    if (mouseDown) {
+        let colorCopy = [... colors]
+        colorCopy[row][col] = "black"
+        setColors(colorCopy)
+    }
+    
 }
 
 export default SearchHolder;
