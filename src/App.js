687de8289d16f02bae4962b45b42ec9c20a76173
Zachary Thomas <zwthomas13@gmail.com>
2021-08-09 15:55:36 -0500

    early dfs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,70 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 
 import SearchHolder from './components/SearchHolder/SearchHolder';
 
 import './App.css';
+import { computeHeadingLevel } from '@testing-library/react';
 
 function App() {
+
+  let dim = 20
+  let template = []
+  let startingColors = []
+  for (let i = 0; i < dim; i++) {
+    let tempColors = []
+    for (let j = 0; j < dim; j++) {
+      tempColors.push("white")
+    }
+    startingColors.push(tempColors)
+  }
+
+  let [row, col] = randomPlacement(startingColors, "green", dim)
+  let [startRow, setStartRow] = useState(row)
+  let [startCol, setStartCol] = useState(col)
+
+  randomPlacement(startingColors, "red", dim)
+  let [colors, setColors] = useState(startingColors)
+  let [mouseDown, setMouseDown] = useState(false);
+
+  for (let i = 0; i < dim; i++) {
+    template.push(<div className="grid" />)
+  }
   return (
     <div className="App">
-      <SearchHolder />
+      <SearchHolder colors={colors} setColors={setColors} mouseDown={mouseDown} setMouseDown={setMouseDown} template={template}/>
+      <button className="start" onClick={() => dfs(colors, setColors, startRow, startCol, dim, 0)}>Start</button>
     </div>
   );
+}
+
+function randomPlacement(colors, color, dim) {
+  let row = Math.floor(Math.random() * dim)
+  let col = Math.floor(Math.random() * dim)
+  
+  colors[row][col] = color
+  return [row, col]
+} 
+
+async function dfs(colors, setColors, row, col, dim, depth) {  
+  // console.log("Depth: " + depth)
+  if (row < 0 || row >= dim || col < 0 || col >= dim) { return false }
+  let currentColor = colors[row][col]
+  if (currentColor === "black" || (currentColor === "green" && depth > 0) || currentColor === "blue") { return false}
+  if ( currentColor === "red") {console.log("solution"); return true}
+  let colorCopy = [... colors]
+  colorCopy[row][col] = "blue"
+  setColors(colorCopy)
+  await sleep(50);
+  let found = false
+  found = await dfs(colors, setColors, row - 1, col, dim, depth + 1)
+  if (!found) {found = await dfs(colors, setColors, row, col - 1, dim, depth + 1)}
+  if (!found) {found = await dfs(colors, setColors, row + 1, col, dim, depth + 1)}
+  if (!found) {found = await dfs(colors, setColors,row, col + 1 , dim, depth + 1)}
+  return found
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 export default App;
--- src/components/SearchHolder/SearchHolder.js
+++ src/components/SearchHolder/SearchHolder.js
@@ -1,32 +1,13 @@
-import React, { useState } from 'react';
 import "./SearchHolder.css"
 
 
-function SearchHolder() {
-    
-    let [color, setColor] = useState("blue")
-    let dim = 20
-    let template = []
-    let startingColors = []
-    for (let i = 0; i < dim; i++) {
-        let tempColors = []
-        for (let j = 0; j < dim; j++) {
-            tempColors.push("white")
-        }
-        startingColors.push(tempColors)
-    }
-    let [colors, setColors] = useState(startingColors)
-    let [mouseDown, setMouseDown] = useState(false);
-
-    for (let i = 0; i < dim; i++) {
-        template.push(<div className="grid"/>)
-    }
+function SearchHolder(props) {
     return (
         <div className="search">
-            {template.map((element, row) => (
-                <div className="grid-row">
-                    {template.map((element, col) => (
-                        <div className="grid" row={row} col={col} style={{"backgroundColor": colors[row][col]}} onMouseEnter={(e) => {handleMouseEnter(mouseDown, e, colors, setColors)}} onMouseDown={(e) => {setMouseDown(true)}} onMouseUp={(e) => {setMouseDown(false)}}/>
+            {props.template.map((element, row) => (
+                <div className="grid-row" key={"row" + row}>
+                    {props.template.map((element, col) => (
+                        <div className="grid" key={"row" + row +"col" + col} row={row} col={col} style={{"backgroundColor": props.colors[row][col]}} onMouseEnter={(e) => {handleMouseEnter(props.mouseDown, e, props.colors, props.setColors)}} onMouseDown={(e) => {props.setMouseDown(true)}} onMouseUp={(e) => {props.setMouseDown(false)}}/>
                     ))}
                 </div>
             
@@ -40,13 +21,15 @@ function SearchHolder() {
 function handleMouseEnter(mouseDown, e, colors, setColors) {
     let row = e.target.attributes.row.value
     let col = e.target.attributes.col.value
-
-    if (mouseDown) {
+    let onColor = colors[row][col]
+    if (mouseDown && onColor != "green" && onColor != "red") {
         let colorCopy = [... colors]
         colorCopy[row][col] = "black"
         setColors(colorCopy)
     }
     
 }
+
+  
 
 export default SearchHolder;
